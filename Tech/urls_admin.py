e3457a67d746c4cd92a70815a3c2fe6e855fccac
vtobarc <134537188+vtobarc@users.noreply.github.com>
2025-03-07 08:55:12 -0500

    Update urls_admin.py

--- Tech/urls_admin.py
+++ Tech/urls_admin.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import views_admin  # Importa tus vistas para el panel admin personalizado
 
 urlpatterns = [
     path('', views.admin_dashboard, name='admin_dashboard'),
