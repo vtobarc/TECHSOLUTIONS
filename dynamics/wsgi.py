17368db8798340e5ef93fa986cb9f28c5972ed04
vtobarc <134537188+vtobarc@users.noreply.github.com>
2025-03-18 22:07:32 -0500

    Update wsgi.py

--- dynamics/wsgi.py
+++ dynamics/wsgi.py
@@ -14,3 +14,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dynamics.settings')
 
 application = get_wsgi_application()
+
+
+
+# Set Cloudinary environment variables BEFORE importing Django
+os.environ['CLOUDINARY_CLOUD_NAME'] = 'intitulado'
+os.environ['CLOUDINARY_API_KEY'] = '241739348942567'
+os.environ['CLOUDINARY_API_SECRET'] = 'wWcQab-9C_R0poTu8p5aOaAhvSk'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://241739348942567:wWcQab-9C_R0poTu8p5aOaAhvSk@intitulado'
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dynamics.settings')
+
+application = get_wsgi_application()
+
