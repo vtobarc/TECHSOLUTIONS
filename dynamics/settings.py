4fd79eba4015667feca194d6ae80b0d7af3fbf03
vtobarc <134537188+vtobarc@users.noreply.github.com>
2025-03-18 21:34:21 -0500

    Update settings.py

--- dynamics/settings.py
+++ dynamics/settings.py
@@ -181,11 +181,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# O si usas las variables separadas:
+CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
+CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
+CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
 
+# Configuración de Cloudinary Storage
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
+    'API_KEY': CLOUDINARY_API_KEY,
+    'API_SECRET': CLOUDINARY_API_SECRET,
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
